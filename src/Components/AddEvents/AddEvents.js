5ce1cea4d8c752a3e2d097ede6f1fa56ce973b55
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-31 21:28:44 +0600

    upload images in third party hosting like imgbb

--- src/Components/AddEvents/AddEvents.js
+++ src/Components/AddEvents/AddEvents.js
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 
 const AddEvents = () => {
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => console.log(data);
+    const handleImageUpload = event => {
+        console.log(event.target.files[0]);
+        // send image to third party app imgbb
+        const imageData = new FormData();
+        imageData.set('key', 'b8a0776f79e4a37c3c341318f0f61e22');
+        imageData.append('image', event.target.files[0])
+
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+            .then(function (response) {
+                console.log(response.data.data.display_url);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <div>
@@ -11,8 +27,7 @@ const AddEvents = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input name="example" defaultValue="test" ref={register} />
                 <br />
-                <input name="exampleRequired" ref={register({ required: true })} />
-                {errors.exampleRequired && <span>This field is required</span>}
+                <input name="exampleRequired" type="file" onChange={handleImageUpload} />
                 <br />
                 <input type="submit" />
             </form>
